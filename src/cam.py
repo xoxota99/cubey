8bca00fb129928ad67c9ef9f44d78259578396c7
Phil <philippe.desrosiers@gmail.com>
2018-10-15 17:26:28 -0700

    Updated Cube State estimator. Still missing final pixel sampling coordinates in cam.py.

--- src/cam.py
+++ src/cam.py
@@ -1,7 +1,41 @@
-
 import cv2
 import numpy as np
 import os.path
+
+colorCenters = (
+    (np.array([147.,  147.,  147.]),  30., "W"),
+    (np.array([231.,   95.,   30.]),  60., "B"),
+    (np.array([0.,    0.,  255.]), 110., "R"),
+    (np.array([0.,  133.,  217.]),  30., "O"),
+    (np.array([53.,  176.,  176.]),  30., "Y"),
+    (np.array([160.,  198.,   0.]),  30., "G"),
+)
+
+# Map Color codes (OWYBGR) to Face codes (FBTLRB).
+# we take this extra step here, instead of inline in colorCenters, to allow for configuring
+# the "known" position of cube faces at startup. Because we can't scan the center facelet
+# of each side (it is obscured by the gripper), we have to assume that it's a certain color for each side.
+# this is basically a config value.
+COLOR_TO_FACE = {
+    "O": "F",
+    "W": "B",
+    "Y": "T",
+    "B": "L",
+    "G": "R",
+    "R": "B",
+    "X": "X"
+}
+
+DEFAULT_SAMPLE_COORDS = [(243, 50), (385, 58), (397, 67),
+                         (243, 62), (385, 71), (397, 81)]
+
+
+def guessColor(v):
+    for v2, radius, code in colorCenters:
+        d = np.linalg.norm(v - v2)
+        if d < radius:
+            return code
+    return "X"
 
 
 def devIdFromPath(path):
@@ -15,7 +49,63 @@ def devIdFromPath(path):
 class Camera:
     vidcap = None
 
-    def __init__(self, deviceName):
-            vidcap = cv2.VideoCapture(devIdFromPath(deviceName))
-            vidcap.set(3, 160)   # cv2.cv.CV_CAP_PROP_FRAME_WIDTH
-            vidcap.set(4, 120)   # cv2.cv.CV_CAP_PROP_FRAME_HEIGHT
+    sampleCoords = []
+
+    def __init__(self, deviceName, sampleCoords=None):
+        self.vidcap = cv2.VideoCapture(devIdFromPath(deviceName))
+
+        self.vidcap.set(3, 160)   # cv2.cv.CV_CAP_PROP_FRAME_WIDTH
+        self.vidcap.set(4, 120)  # cv2.cv.CV_CAP_PROP_FRAME_HEIGHT
+        # vidcap.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS,       0.20 )#0.50 default
+        # vidcap.set(cv2.cv.CV_CAP_PROP_CONTRAST,         0.15 )#0.15 default
+        # vidcap.set(cv2.cv.CV_CAP_PROP_SATURATION,       0.10 )#0.15 default
+        # vidcap.set(cv2.cv.CV_CAP_PROP_GAIN,             0.15 )#0.15 default
+        # vidcap.set(cv2.cv.CV_CAP_PROP_EXPOSURE, 0.5 ) #not supported by C310
+        # vidcap.set(21, 0.0)#CV_CAP_PROP_AUTO_EXPOSURE #not supported by C310
+
+        if sampleCoords is None:
+            self.sampleCoords = DEFAULT_SAMPLE_COORDS
+        else:
+            self.sampleCoords = sampleCoords
+
+    # Given a camera reference, take an vertical edge-on picture of a rubik's cube, and evaluate some of the facelets.
+    # There are two cameras, Front-Right, and Back-Left.
+    # In the case of the Front-right camera, for example, return a tuple of FURBDL values that refer to the facelets
+    # in the (zero-based) F2, R0, F5, R3, F8, R6 positions.
+    def get_colors(self):
+        arr = []
+
+        camFrames = []
+        _, frame = self.vidcap.read()
+        if frame != None:
+            camFrames.append(frame)
+
+        if len(camFrames) == 0:
+            print("no frames!")
+            return "X", "X", "X", "X", "X", "X"
+
+        camFrame = np.hstack(camFrames)  # stack some frames.
+
+        for coord in self.sampleCoords:
+            x, y = coord
+
+            # define a small square (x1,y1,x2,y2) in the frame to sample for color.
+            x1, y1 = x - 2, y - 2
+            x2, y2 = x + 2, y + 2
+
+            block = camFrame[y1:y2, x1:x2]
+            block_r = block[:, :, 0]    # red
+            block_g = block[:, :, 1]    # green
+            block_b = block[:, :, 2]    # blue
+
+            # get the average color within the block, for each R/G/B component.
+            clr = np.array(
+                (np.median(block_r), np.median(block_g), np.median(block_b)))
+            #clr /= np.linalg.norm( clr ) / 255.0
+
+            code = guessColor(clr)
+            code = COLOR_TO_FACE[code]
+
+            arr.append(code)
+
+        return tuple(arr)
--- src/cubey.py
+++ src/cubey.py
@@ -4,7 +4,7 @@ import subprocess
 import logging
 import stepper
 import numpy as np
-import cam
+from cam import Camera
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -32,14 +32,6 @@ def solve(t=None):
     return subprocess.check_output([CMD, t], stderr=open(os.devnull, 'wb')).strip()
 
 
-# Given a camera reference, take an edge-on picture of a rubik's cube, and evaluate some of the facelets.
-# There are two cameras, Front-Right, and Back-Left.
-# In the case of the Front-right camera, for example, return a tuple of FURBDL values that refer to the facelets
-# in the (zero-based) F2, R0, F5, R3, F8, R6 positions.
-def record_state(cam):
-    return "F", "R", "F", "R", "F", "R"
-
-
 # Record cube state, according to:
 #
 #              |------------|
@@ -63,71 +55,72 @@ def record_state(cam):
 #              |------------|
 #
 def scan_state():
+    # initialize state.
+    global state
+
+    state = {
+        'F': np.full(9, "F"),
+        'B': np.full(9, "B"),
+        'L': np.full(9, "L"),
+        'R': np.full(9, "R"),
+        'U': np.full(9, "U"),
+        'D': np.full(9, "D")
+    }
+
     # scan front
     logging.info("Scanning FRONT")
 
-    state['F'][2], state['R'][0], state['F'][5], state['R'][3], state['F'][8], state['R'][6] = record_state(
-        cam1)
+    state['F'][2], state['R'][0], state['F'][5], state['R'][3], state['F'][8], state['R'][6] = cam1.get_colors()
 
-    stepper.rot_90(stepper.FRONT, stepper.CW)
-    state['F'][0], state['U'][6], state['F'][1], state['U'][7], _, state['U'][8] = record_state(
-        cam1)
+    stepper.rot_90(stepper.FRONT)
+    state['F'][0], state['U'][6], state['F'][1], state['U'][7], _, state['U'][8] = cam1.get_colors()
 
-    stepper.rot_90(stepper.FRONT, stepper.CW)
-    state['F'][6], state['L'][8], state['F'][3], state['L'][5], _, state['L'][2] = record_state(
-        cam1)
+    stepper.rot_90(stepper.FRONT)
+    state['F'][6], state['L'][8], state['F'][3], state['L'][5], _, state['L'][2] = cam1.get_colors()
 
-    stepper.rot_90(stepper.FRONT, stepper.CW)
-    _, state['D'][2], state['F'][7], state['D'][1], _, state['D'][0] = record_state(
-        cam1)
+    stepper.rot_90(stepper.FRONT)
+    _, state['D'][2], state['F'][7], state['D'][1], _, state['D'][0] = cam1.get_colors()
 
-    stepper.rot_90(stepper.FRONT, stepper.CW)   # back to origin
+    stepper.rot_90(stepper.FRONT)   # back to origin
 
     # scan right
     logging.info("Scanning RIGHT")
-    stepper.rot_90(stepper.RIGHT, stepper.CW)
-    _, _, state['D'][5], state['R'][7], state['D'][8], state['R'][8] = record_state(
-        cam1)
+    stepper.rot_90(stepper.RIGHT)
+    _, _, state['D'][5], state['R'][7], state['D'][8], state['R'][8] = cam1.get_colors()
 
-    stepper.rot_90(stepper.RIGHT, stepper.CW)
-    state['B'][6], _, state['B'][3], state['R'][5], state['B'][0], state['R'][2] = record_state(
-        cam1)
+    stepper.rot_90(stepper.RIGHT)
+    state['B'][6], _, state['B'][3], state['R'][5], state['B'][0], state['R'][2] = cam1.get_colors()
 
-    stepper.rot_90(stepper.RIGHT, stepper.CW)
-    state['U'][2], _, state['U'][5], state['R'][1], _, _ = record_state(cam1)
+    stepper.rot_90(stepper.RIGHT)
+    state['U'][2], _, state['U'][5], state['R'][1], _, _ = cam1.get_colors()
 
-    stepper.rot_90(stepper.RIGHT, stepper.CW)  # back to origin
+    stepper.rot_90(stepper.RIGHT)  # back to origin
 
     # scan back
     logging.info("Scanning BACK")
-    state['B'][2], state['L'][0], state['B'][5], state['L'][3], state['B'][8], state['L'][6] = record_state(
-        cam2)
+    state['B'][2], state['L'][0], state['B'][5], state['L'][3], state['B'][8], state['L'][6] = cam2.get_colors()
 
-    stepper.rot_90(stepper.BACK, stepper.CW)
-    _, _, state['B'][1], state['U'][1], _, state['U'][0] = record_state(cam2)
+    stepper.rot_90(stepper.BACK)
+    _, _, state['B'][1], state['U'][1], _, state['U'][0] = cam2.get_colors()
 
-    # stepper.rot_90(stepper.BACK, stepper.CW)
-    # _, _, _, _, _, _ = record_state(cam2)
+    # skip one edge. We've got the data already.
 
-    # stepper.rot_90(stepper.BACK, stepper.CW)
-    stepper.rot_180(stepper.BACK, stepper.CW)
-    _, state['D'][6], state['B'][7], state['D'][7], _, _ = record_state(cam2)
+    stepper.rot_180(stepper.BACK)
+    _, state['D'][6], state['B'][7], state['D'][7], _, _ = cam2.get_colors()
 
-    stepper.rot_90(stepper.BACK, stepper.CW)   # back to origin
+    stepper.rot_90(stepper.BACK)   # back to origin
 
     # scan left
     logging.info("Scanning LEFT")
-    stepper.rot_90(stepper.LEFT, stepper.CW)
-    _, _, state['D'][3], state['L'][7], _, _ = record_state(cam2)
+    stepper.rot_90(stepper.LEFT)
+    _, _, state['D'][3], state['L'][7], _, _ = cam2.get_colors()
 
-    # stepper.rot_90(stepper.BACK, stepper.CW)
-    # _, _, _, _, _, _ = record_state(cam2)
+    # skip one edge. We've got the data already.
 
-    # stepper.rot_90(stepper.RIGHT, stepper.CW)
-    stepper.rot_180(stepper.RIGHT, stepper.CW)
-    _, _, state['U'][3], state['L'][1], _, _ = record_state(cam2)
+    stepper.rot_180(stepper.RIGHT)
+    _, _, state['U'][3], state['L'][1], _, _ = cam2.get_colors()
 
-    stepper.rot_90(stepper.RIGHT, stepper.CW)  # back to origin
+    stepper.rot_90(stepper.RIGHT)  # back to origin
 
 
 def get_state_string():
