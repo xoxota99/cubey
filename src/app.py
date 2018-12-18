70bce20860858a7994a2609e66eb28cbf220ecbc
Phil Desrosiers <philippe.desrosiers@gmail.com>
2018-12-18 14:49:17 -0800

    Added interactive mode.

--- src/app.py
+++ src/app.py
@@ -5,24 +5,13 @@ import logging
 import time
 import yaml
 
-from cubey import stepper
+from cubey.stepper import MotorController
 from cubey.scanner import Scanner
 from cubey.solver import Solver
-# from cubey.config import cfg
 
 
-def main():
+def solve(scanner, solver, motors):
     """scan the cube, get a solution from kociemba, then execute it on the robot."""
-    config_file = "config.yaml"
-    config = {}
-    with open(config_file, 'r') as ymlfile:
-        config = yaml.load(ymlfile)
-
-    logging.basicConfig(
-        level=logging.getLevelName(config['app']['logLevel']), format=config['app']['logFormat'])
-    scanner = Scanner(config)
-    solver = Solver(config)
-
     logging.info("Scanning...")
     t0 = round(time.time() * 1000)
     state_str = scanner.get_state_string(scanner.scan_state())
@@ -39,7 +28,7 @@ def main():
 
     logging.info("Executing...\n")
     t4 = round(time.time() * 1000)
-    stepper.execute(solution)
+    motors.execute(solution)
     t5 = round(time.time() * 1000)
 
     logging.info('Scan time: {:d}ms'.format(t1-t0))
@@ -49,5 +38,50 @@ def main():
     return(0)
 
 
+def solve_interactive(scanner, solver, motors):
+    """scan the cube, get a solution from kociemba, then execute it on the robot."""
+    input("Press any key to begin scanning the cube.")
+    t0 = round(time.time() * 1000)
+    state_str = scanner.get_state_string(scanner.scan_state())
+    t1 = round(time.time() * 1000)
+    print("Scan complete! Cube state: {0}\n".format(state_str))
+
+    input("Press any key to generate the solution.")
+
+    t2 = round(time.time() * 1000)
+    solution = solver.solve(state_str)
+    t3 = round(time.time() * 1000)
+    print("Finished solving! Solution: {0}\n".format(solution))
+
+    input("Press any key to execute the solution.")
+
+    t4 = round(time.time() * 1000)
+    motors.execute(solution)
+    t5 = round(time.time() * 1000)
+
+    print('Scan time: {:d}ms'.format(t1-t0))
+    print('Solve time: {:d}ms'.format(t3-t2))
+    print('Execution time: {:d}ms'.format(t5-t4))
+
+    return(0)
+
+
 if __name__ == "__main__":
-    sys.exit(main())
+    config_file = "config.yaml"
+    config = {}
+    with open(config_file, 'r') as ymlfile:
+        config = yaml.load(ymlfile)
+
+    logging.basicConfig(
+        level=logging.getLevelName(config['app']['logLevel']), format=config['app']['logFormat'])
+    scanner = Scanner(config)
+    solver = Solver(config)
+    motors = MotorController(config)
+
+    func = solve
+
+    if len(sys.argv) > 1:
+        if sys.argv[1].upper() == "-I":
+            func = solve_interactive
+
+    sys.exit(func(scanner, solver, motors))
