0c594bd052e791d68ab9783822d37ac89196c049
Phil Desrosiers <philippe.desrosiers@gmail.com>
2021-09-09 15:46:08 -0700

    reduce complexity in calibration

--- src/calibrate.py
+++ src/calibrate.py
@@ -30,46 +30,44 @@ camera = camera.Camera(config, calib)
 
 def collision_detect(config):
     collisions = []
-    compared = {"F": [], "U": [], "R": [], "B": [], "L": [], "D": []}
-    cal = config["facelet_hsv"]
+    keys = ["F", "U", "R", "B", "L", "D"]
+    cal = config["colors"]
 
-    for key1 in cal:  # 6
-        bracket1 = cal[key1]
-        for key2 in cal:  # 5, 4, 3, 2, 1
+    for i in range(0, 6):
+        bracket1 = cal[keys[i]]
+        for j in range(i + 1, 6):
+            bracket2 = cal[keys[j]]
             collide = False
-            compared[key1].append(key2)
-            if key2 != key1 and key1 not in compared[key2]:
-                bracket2 = cal[key2]
-                # print("#Testing {0} against {1}".format(key1, key2))
-                if(bracket1["max"][0] < bracket1["min"][0] or bracket2["max"][0] < bracket2["min"][0]):
-                    # special case (red). Max H value is less than min.
-                    red = bracket1
-                    nonred = bracket2
-                    if bracket2[1][0] < bracket2[0][0]:  # swap
-                        red = bracket2
-                        nonred = bracket1
 
-                    collide = (red["max"][0] > nonred["min"][0]
-                               or red["min"][0] < nonred["max"][0])
-                else:
-                    # non-red. Do these hues overlap?
-                    collide = (bracket1["max"][0] > bracket2["min"][0])
+            if(bracket1["max"][0] < bracket1["min"][0] or bracket2["max"][0] < bracket2["min"][0]):
+                # special case (red). Max H value is less than min.
+                red = bracket1
+                nonred = bracket2
+                if bracket2[1][0] < bracket2[0][0]:  # swap
+                    red = bracket2
+                    nonred = bracket1
 
-                # now check S and V, which are the same for red and non-red.
-                collide = collide and (
-                    bracket1["min"][0] < bracket2["max"][0]) and (
-                    bracket1["max"][1] > bracket2["min"][1]) and (
-                    bracket1["min"][1] < bracket1["max"][1]) and (
-                    bracket1["max"][2] > bracket2["min"][2]) and (
-                    bracket1["min"][2] < bracket2["max"][2])
+                collide = (red["max"][0] > nonred["min"][0]
+                           or red["min"][0] < nonred["max"][0])
+            else:
+                # non-red. Do these hues overlap?
+                collide = (bracket1["max"][0] > bracket2["min"][0])
 
-                if collide:
-                    collisions.append(
-                        {
-                            key1: [bracket1["min"], bracket1["max"]],
-                            key2: [bracket2["min"], bracket2["max"]],
-                        }
-                    )
+            # now check S and V, which are the same for red and non-red.
+            collide = collide and (
+                bracket1["min"][0] < bracket2["max"][0]) and (
+                bracket1["max"][1] > bracket2["min"][1]) and (
+                bracket1["min"][1] < bracket1["max"][1]) and (
+                bracket1["max"][2] > bracket2["min"][2]) and (
+                bracket1["min"][2] < bracket2["max"][2])
+
+            if collide:
+                collisions.append(
+                    {
+                        keys[i]: [bracket1["min"], bracket1["max"]],
+                        keys[j]: [bracket2["min"], bracket2["max"]],
+                    }
+                )
 
     if len(collisions) > 0:
         logging.warn("-----------------------------------------------")
