d38f3154ae09e6499c60faa93ae4ed197860d279
Phil Desrosiers <philippe.desrosiers@gmail.com>
2025-05-30 13:25:03 -0400

    Add detailed docstrings and refactor global variables into class attributes

--- src/lib/motorcontroller.py
+++ src/lib/motorcontroller.py
@@ -2,46 +2,46 @@ from time import sleep
 import pigpio
 import logging
 from lib.logger import setup_logging
+from typing import Dict, Optional, Union, List, Tuple, Any
 
 """
 Controller for stepper motors.
 """
 
-config = {}
-DIR_PIN = 0          # GPIO pin to set direction (cw / ccw)
-DISABLE_PIN = 0      # GPIO pin to disable the stepper
-FACE_MOTOR_MAP = {}  # map of faces to motor pins.
-
-STEPS_PER_DEGREE = 200  # steps per degree (varies by motor)
-STEP_FACTOR = 8  # number of pulses per step. (for microstepping)
-MOVE_DELAY = 0.05  # take a small break beween stepper movements.
-HERTZ = 400
-
-CW = 1
-CCW = 0
-
-pi = None       # pigpio reference
-is_init = False
-
-
 class MotorController:
-
-    def __init__(self, _config):
-        global config, DIR_PIN, DISABLE_PIN, FACE_MOTOR_MAP, STEPS_PER_DEGREE, STEP_FACTOR, MOVE_DELAY, HERTZ
-
-        config = _config
-
-        pins = _config['stepper']['pin_map']
-
+    """
+    Controller for stepper motors that manipulate the Rubik's Cube.
+    
+    This class provides methods to control the stepper motors that rotate the faces
+    of the Rubik's Cube. It uses the pigpio library to control the GPIO pins on the
+    Raspberry Pi.
+    """
+    
+    # Class constants
+    CW = 1  # Clockwise direction
+    CCW = 0  # Counter-clockwise direction
+    
+    def __init__(self, config: Dict[str, Any]):
+        """
+        Initialize the MotorController with the given configuration.
+        
+        Args:
+            config: Dictionary containing configuration parameters
+        """
+        self.__config = config
+        self.__pi = None
+        self.__is_init = False
+        
+        pins = config['stepper']['pin_map']
+        
         # Direction GPIO Pin
-        DIR_PIN = pins['dir']
-
+        self.__dir_pin = pins['dir']
+        
         # Disable GPIO pin
-        if 'disable' in pins:
-            DISABLE_PIN = pins['disable']
-
-        # map of faces to GPIO pins
-        FACE_MOTOR_MAP = {
+        self.__disable_pin = pins.get('disable', 0)
+        
+        # Map of faces to GPIO pins
+        self.__face_motor_map = {
             "U": pins['up'],
             "R": pins['right'],
             "F": pins['front'],
@@ -49,118 +49,138 @@ class MotorController:
             "L": pins['left'],
             "B": pins['back']
         }
+        
+        # Number of stepper motor "steps" per degree of angle
+        self.__steps_per_degree = config['stepper']['steps_per_rev'] / 360.0
+        
+        # Micro-stepping factor
+        self.__step_factor = config['stepper']['step_factor']
+        
+        # Seconds to wait between cube moves
+        self.__move_delay = config['stepper']['move_delay']
+        
+        # Pulses per second (speed)
+        self.__hertz = config['stepper']['hertz']
 
-        # Number of stepper motor "steps" per degree of angle.
-        STEPS_PER_DEGREE = config['stepper']['steps_per_rev'] / 360.0
-
-        # micro-stepping factor.
-        STEP_FACTOR = config['stepper']['step_factor']
-
-        # seconds to wait between cube moves.
-        MOVE_DELAY = config['stepper']['move_delay']
-
-        HERTZ = config['stepper']['hertz']
-
-    def _initialize(self, force=False):
+    def _initialize(self, force: bool = False) -> None:
         """
-        _initialize stepper motor frequency / PWM ramps for 90 and 180-degree turns
+        Initialize stepper motor frequency / PWM ramps for 90 and 180-degree turns.
+        
+        Args:
+            force: If True, reinitialize even if already initialized
+        
+        Raises:
+            Exception: If initialization fails
         """
-        global pi, is_init
-
         try:
             # Connect to pigpiod daemon
-            pi = pigpio.pi()
+            self.__pi = pigpio.pi()
             sleep(0.001)
-
-            if force or not is_init:
-                if DISABLE_PIN:
-                    pi.set_mode(DISABLE_PIN, pigpio.OUTPUT)
-                    pi.write(DISABLE_PIN, 1)
-
-                # Set up pins as an output
-                pi.set_mode(DIR_PIN, pigpio.OUTPUT)
-                for _, val in config['stepper']['pin_map'].items():
-                    pi.set_mode(val, pigpio.OUTPUT)
-
-                is_init = True
+            
+            if force or not self.__is_init:
+                if self.__disable_pin:
+                    self.__pi.set_mode(self.__disable_pin, pigpio.OUTPUT)
+                    self.__pi.write(self.__disable_pin, 1)
+                
+                # Set up pins as outputs
+                self.__pi.set_mode(self.__dir_pin, pigpio.OUTPUT)
+                for _, val in self.__config['stepper']['pin_map'].items():
+                    self.__pi.set_mode(val, pigpio.OUTPUT)
+                
+                self.__is_init = True
         except Exception as e:
             logging.error(f"Failed to initialize motor controller: {e}")
             self._stop()
             raise
-
-    def _stop(self):
-        global is_init
-        if is_init:
-            pi.stop()
+    
+    def _stop(self) -> None:
+        """Stop the motor controller and release resources."""
+        if self.__is_init and self.__pi is not None:
+            self.__pi.stop()
             sleep(0.001)
-            is_init = False
-
-    def _tx_pulses(self, pin, hertz, num):
-        assert hertz < 500000
-
-        # length of shortest possible pulse, in microseconds.
+            self.__is_init = False
+    
+    def _tx_pulses(self, pin: int, hertz: int, num: int) -> None:
+        """
+        Transmit pulses to the stepper motor.
+        
+        Args:
+            pin: GPIO pin to pulse
+            hertz: Frequency of pulses
+            num: Number of pulses to send
+        
+        Raises:
+            AssertionError: If hertz is too high or num is too large
+        """
+        assert hertz < 500000, "Hertz value too high"
+        assert num < 65536, "Number of pulses too large"
+        
+        # Length of shortest possible pulse, in microseconds
         length_us = int(1000000 / hertz)
-
-        assert num < 65536
-
-        if DISABLE_PIN:
-            pi.write(DISABLE_PIN, 0)
-            sleep(0.001)       # one millisecond
-
+        
+        if self.__disable_pin:
+            self.__pi.write(self.__disable_pin, 0)
+            sleep(0.001)  # One millisecond
+        
         num_low = num % 256
         num_high = num // 256
-
+        
         waveform = []
-
-        waveform.append(pigpio.pulse(gpio_on=1 << pin,
-                        gpio_off=0, delay=1))    # rising edge
-        waveform.append(pigpio.pulse(
-            gpio_on=0, gpio_off=1 << pin, delay=length_us - 1))     # falling edge
-
-        pi.wave_add_generic(waveform)
-
-        wid = pi.wave_create()
-
+        
+        # Rising edge
+        waveform.append(pigpio.pulse(gpio_on=1 << pin, gpio_off=0, delay=1))
+        # Falling edge
+        waveform.append(pigpio.pulse(gpio_on=0, gpio_off=1 << pin, delay=length_us - 1))
+        
+        self.__pi.wave_add_generic(waveform)
+        
+        wid = self.__pi.wave_create()
+        
         if wid >= 0:
-            pi.wave_chain([255, 0, wid, 255, 1, num_low, num_high])
-            while pi.wave_tx_busy():
+            self.__pi.wave_chain([255, 0, wid, 255, 1, num_low, num_high])
+            while self.__pi.wave_tx_busy():
                 pass
-            pi.wave_delete(wid)
+            self.__pi.wave_delete(wid)
+        
+        if self.__disable_pin:
+            self.__pi.write(self.__disable_pin, 1)
+            sleep(0.001)  # One millisecond
 
-        if DISABLE_PIN:
-            pi.write(DISABLE_PIN, 1)
-            sleep(0.001)       # one millisecond
-
-    def rot_90(self, motor_pin, direction=CW):
+    def rot_90(self, motor_pin: int, direction: int = CW) -> None:
         """
-        rotate 90 degrees in the specified direction (CW or CCW)
-        param:motor_pin - One of UP, RIGHT, FRONT, DOWN, LEFT, or BACK, as defined above.
-        param:direction - One of CW or CCW, as defined above.
+        Rotate 90 degrees in the specified direction.
+        
+        Args:
+            motor_pin: GPIO pin for the motor to rotate
+            direction: Direction to rotate (CW or CCW)
         """
-        if not is_init:
+        if not self.__is_init:
             self._initialize()
-        pi.write(DIR_PIN, direction)
-        steps = int(90 * STEPS_PER_DEGREE * STEP_FACTOR)
-
-        self._tx_pulses(motor_pin, HERTZ, steps)
-
-    def rot_180(self, motor_pin, direction=CW):
+        
+        self.__pi.write(self.__dir_pin, direction)
+        steps = int(90 * self.__steps_per_degree * self.__step_factor)
+        
+        self._tx_pulses(motor_pin, self.__hertz, steps)
+    
+    def rot_180(self, motor_pin: int, direction: int = CW) -> None:
         """
-        rotate 180 degrees in the specified direction (CW or CCW)
-        param:motor_pin - One of UP, RIGHT, FRONT, DOWN, LEFT, or BACK, as defined above.
-        param:direction - One of CW or CCW, as defined above. Default is CW
+        Rotate 180 degrees in the specified direction.
+        
+        Args:
+            motor_pin: GPIO pin for the motor to rotate
+            direction: Direction to rotate (CW or CCW)
         """
-        if not is_init:
+        if not self.__is_init:
             self._initialize()
-        pi.write(DIR_PIN, direction)
-        steps = int(180 * STEPS_PER_DEGREE * STEP_FACTOR)
-
-        self._tx_pulses(motor_pin, HERTZ, steps)
-
-    def execute(self, recipe_str):
+        
+        self.__pi.write(self.__dir_pin, direction)
+        steps = int(180 * self.__steps_per_degree * self.__step_factor)
+        
+        self._tx_pulses(motor_pin, self.__hertz, steps)
+    
+    def execute(self, recipe_str: str) -> bool:
         """
-        Take a recipe, of the form (e.g.) "R L2 F B U' F' D F' U B2 L' U2 B2 U D' B2 U2 L2 D' R2 D2"
-        and execute it using the attached stepper motors.
+        Take a recipe in cube notation and execute it using the attached stepper motors.
         
         Args:
             recipe_str: String containing cube notation moves to execute
@@ -175,11 +195,11 @@ class MotorController:
             logging.error(f"Invalid recipe: {recipe_str}")
             return False
 
-        if not is_init:
+        if not self.__is_init:
             self._initialize()
             
         recipe = recipe_str.split()
-        valid_faces = set(FACE_MOTOR_MAP.keys())
+        valid_faces = set(self.__face_motor_map.keys())
         valid_modifiers = set(['', '\'', '2'])
         
         # Validate the recipe before executing
@@ -203,15 +223,15 @@ class MotorController:
         for step_str in recipe:
             base = step_str[0]
             # TODO: We can execute opposite sides simultaneously, if the NEXT item in the list is OPPOSITE this item AND has the SAME orientation (CW or CCW) as this item.
-            pin = FACE_MOTOR_MAP[base]
+            pin = self.__face_motor_map[base]
             if (len(step_str) >= 2):
                 xtra = step_str[-1:]
                 if (xtra == "'"):
-                    self.rot_90(pin, CCW)
+                    self.rot_90(pin, self.CCW)
                 elif (xtra == "2"):
                     self.rot_180(pin)
             else:
                 self.rot_90(pin)
-            sleep(MOVE_DELAY)
+            sleep(self.__move_delay)
         self._stop()
         return True
